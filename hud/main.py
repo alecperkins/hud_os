073d34bfac4a9c83524777408a71b4451fdcc962
Alec Perkins <mail@alecperkins.me>
2021-02-06 09:39:41 -0500

    3

--- hud/main.py
+++ hud/main.py
@@ -1,98 +1,46 @@
-
-from epdconfig import RaspberryPi
 from display import EPD
-from renderer import Renderer
+from epdconfig import RaspberryPi
+#from fetch_data import loadOrFetchData
+#from renderer import Renderer
+from time import sleep
 
 driver = RaspberryPi()
 display = EPD(driver)
-renderer = Renderer(display)
+# renderer = Renderer(display)
 
-from random import randint
-data = {
-    'line1': randint(0,100000),
-    'line2': randint(0,100000),
-}
-renderer.render(data)
+from cairosvg import svg2png
+import io
+from PIL import Image
 
+print('initialized')
 
+img_bytes = svg2png(url='./test.svg')
 
+print(len(img_bytes), 'bytes')
 
+width = 800
+height = 480
+print(width, height)
+graphic = Image.open(io.BytesIO(img_bytes))
+print(graphic)
 
+img_b = Image.new('1', (width, height), 255)
+img_r = Image.new('1', (width, height), 255)
 
+print(img_b, img_r)
 
+print('compositing')
+img_b.paste(graphic)
 
+print('connecting')
+display.init()
+print('displaying')
+display.show(display.getbuffer(img_b), display.getbuffer(img_r))
+print('disconnecting')
+display.sleep()
+print('done')
+# while True:
+#     data = loadOrFetchData
+#     renderer.render(data)
+#     sleep(60)
 
-
-
-
-# Bimage = Image.new('1', (display.width, display.height), 255)  # 255: clear the frame
-# Rimage = Image.new('1', (display.width, display.height), 255)
-# draw_black = ImageDraw.Draw(Bimage)
-# draw_red = ImageDraw.Draw(Rimage)
-
-
-# font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-# draw_black.text((10, 0), 'Hello, world!', font = font24, fill = 0)
-# draw_red.text((10, 30), 'Hello, world?', font = font24, fill = 0)
-
-
-# display.init()
-# display.show(display.getbuffer(Bimage), display.getbuffer(Rimage))
-# display.sleep()
-
-
-
-
-
-# from random import randint
-
-# Bimage = Image.new('1', (display.width, display.height), 255)  # 255: clear the frame
-# Rimage = Image.new('1', (display.width, display.height), 255)
-# draw_black = ImageDraw.Draw(Bimage)
-# draw_red = ImageDraw.Draw(Rimage)
-
-
-# font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-# draw_black.text((10, 0), 'Hello, world! ' + str(randint(0,10000)), font = font24, fill = 0)
-# draw_red.text((10, 30), 'Hello, world?', font = font24, fill = 0)
-
-
-# display.init()
-# display.display(display.getbuffer(Bimage), display.getbuffer(Rimage))
-# display.sleep()
-
-
-
-
-# # import sys
-# # import os
-# # picdir = '/home/pi/epaper-libs/e-Paper/RaspberryPi_JetsonNano/python/pic'
-# # libdir = '/home/pi/epaper-libs/e-Paper/RaspberryPi_JetsonNano/python/lib'
-# # if os.path.exists(libdir):
-# #     sys.path.append(libdir)
-
-# # import logging
-# # from waveshare_epd import epd7in5b_V2
-# # # from waveshare_epd import epd7in5
-# # import time
-# # from PIL import Image,ImageDraw,ImageFont
-# # import traceback
-
-
-# # epd = epd7in5b_V2.EPD()
-# # # epd = epd7in5bc.EPD()
-# # epd.init()
-# # epd.Clear()
-# # epd.sleep()
-
-# # Bimage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-# # Rimage = Image.new('1', (epd.width, epd.height), 255)
-# # draw_black = ImageDraw.Draw(Bimage)
-# # draw_red = ImageDraw.Draw(Rimage)
-
-
-# # font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-# # draw_black.text((10, 0), 'Hello, world!', font = font24, fill = 0)
-# # draw_red.text((10, 30), 'Hello, world?', font = font24, fill = 0)
-
-# # epd.init();epd.display(epd.getbuffer(Bimage), epd.getbuffer(Rimage));epd.sleep()
